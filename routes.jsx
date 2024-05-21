43fd429ab5907a0af006b04e3258c6d63271ff42
Sanskar1264 <163060964+Sanskar1264@users.noreply.github.com>
2024-05-22 03:29:34 +0530

    routes.jsx

---
+++ routes.jsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignUpPage from "./signUp";
+import LoginPage from "./login";
+import HomePage from "./home";
+import RootLayout from "../rootLayout";
+const Routes = (props)=>{
+    const router = createBrowserRouter([
+        {
+            path: "/home",
+            element : <RootLayout loginStatus = {props.loginStatus} setLoginStatus = {props.setLoginStatus}/>,
+            children: [
+                {
+                    path:"/home",
+                    element:<HomePage loginStatus = {props.loginStatus}/>
+                },
+                {
+                    path: "/home/signup",
+                    element: <SignUpPage/>
+                },
+                {
+                    path: "/home/signin",
+                    element: <LoginPage setLoginStatus = {props.setLoginStatus}/>
+                }
+            ]
+        },
+        
+    ])
+
+    return (
+        <RouterProvider router={router}/>
+    );
+
+};
+export default Routes;
